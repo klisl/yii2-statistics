035c83596fa2bde2c185ee7c6693eeb3fa90e5e1
Sergey Klimenchuk <ksl1980@mail.ru>
2017-08-14 15:21:26 +0300

    start

---
+++ src/Module.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Klisl\Statistics;
+
+use yii\base\BootstrapInterface;
+use yii\base\Module as BaseModule;
+
+
+class Module extends BaseModule implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+
+//        $app->on(\yii\web\Controller::EVENT_AFTER_ACTION, function () {
+//            // остальной код
+//            exit('Готово');
+//        });
+    }
+}
